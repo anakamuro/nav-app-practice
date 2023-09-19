e11607b26748afe1a3fb77b99d11e46f556cced4
Akihiko Nakamura <anakamuro@earthlink.net>
2023-09-19 19:36:52 -0400

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+//   
+links.classList.toggle("show-links");
+});
